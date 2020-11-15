8ac7befccd11bd939f85dcdf9fe3bf6f148f3047
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2020-11-14 21:07:30 -0500

    Delete time_diff.sql

--- time_diff.sql
+++
@@ -1,7 +0,0 @@
-set @time_diff := (select timediff(current_timestamp(), (select verified_date from capstone.device where is_verified = 1)));
-
-select @time_diff;
-
-set @is_expired := (select if(@time_diff >= "00:30:00", "yes","no"));
-
-select @is_expired;
